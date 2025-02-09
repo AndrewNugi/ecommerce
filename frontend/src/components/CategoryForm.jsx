fb57c3d00b830ae648a0b9e678c352e545ad3989
Andrew Oskar <andrewoskar@Beatrices-MacBook-Air.local>
2025-02-09 11:59:10 +0300

    Added css for CategoryList, CategoryForm and Modal

--- frontend/src/components/CategoryForm.jsx
+++ frontend/src/components/CategoryForm.jsx
@@ -1,3 +1,4 @@
+import './CategoryForm.css'
 
 const CategoryForm = ({
     value,
@@ -7,24 +8,22 @@ const CategoryForm = ({
     handleDelete,
 }) => {
   return (
-    <div className="p-3">
-        <form onSubmit={handleSubmit} className="space-y-3">
+    <div className="form-container">
+        <form onSubmit={handleSubmit} className="form">
             <input 
             type="text" 
-            className="py-3 px-4 border rounded-lg w-full" 
+            className="input-style" 
             placeholder="Write category name"
             value={value}
             onChange={(e) => setValue(e.target.value)}
             />
 
             <div className="flex justfy-between">
-                <button className="bg-pink-500 text-white py-2 px-4 rounded-lg hover:bg-pink600 
-                focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50">{buttonText}
+                <button className="btn-style btn-pink">{buttonText}
                 </button>
 
                 {handleDelete && (
-                    <button onClick={handleDelete} className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 
-                    focus:outline-none focus:ring-2focus:ring-red-500 focus:ring-opacity-50">
+                    <button onClick={handleDelete} className="btn-style btn-red">
                         Delete
                     </button>
                 )}
--- frontend/src/pages/admin/CategoryList.jsx
+++ frontend/src/pages/admin/CategoryList.jsx
@@ -6,6 +6,7 @@ import { useCreateCategoryMutation,
     useGetAllCategoriesQuery } from '../../redux/api/categoryApiSlice'
 import CategoryForm from "../../components/CategoryForm"
 import Modal from '../../components/Modal'
+import './CategoryList.css'
 
 const CategoryList = () => {
     const {data: categories } = useGetAllCategoriesQuery()
@@ -86,21 +87,18 @@ const CategoryList = () => {
 
 
   return (
-    <div className="ml-[10rem] flex flex-col md:flex-row">
+    <div className="main-container">
         {/*Admin Menu*/}
-        <div className="w-3/4 p-3">
-            <div className="h-12">Manage Categories</div>
+        <div className="form-container">
+            <div className="h1-style">Manage Categories</div>
             <CategoryForm value={name} setValue={setName} handleSubmit={handleCreateCategory} />
             <br />
             <hr />
 
-            <div className="flex flex-wrap">
+            <div className="categories">
                 {categories?.map((category) => (
                     <div key={category._id}>
-                        <button className="bg-white border border-pink-500 
-                        text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 
-                        hover:text-white focus:outline-none focus:ring-2
-                        focus:ring-pink-500 focus:ring-opacity-50" onClick={() => {{
+                        <button className="category-button" onClick={() => {{
                             setModalVisible(true)
                             setSelectedCategory(category)
                             setUpdateName(category.name)
