b6e746a86058751606db7945cab27c27434576c2
Andrew Oskar <andrewoskar@Beatrices-MacBook-Air.local>
2025-02-13 20:02:27 +0300

    Added CRUD functionality + ability to upload photo for product

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import formidable from 'express-formidable'
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+import { addProduct, updateProduct, removeProduct, fetchProducts, fetchProductById, fetchAllProducts, addProductReviews, fetchTopProduct, fetchNewProduct } from "../controllers/productController.js";
+
+
+const router = express.Router();
+
+router.route('/')
+.post(authenticate, authorizeAdmin, formidable(), addProduct)
+.get(fetchProducts)
+
+router.route('/allProducts').get(fetchAllProducts)
+router.route('/:id/reviews').post(authenticate, authorizeAdmin, checkId, addProductReviews)
+
+router.get('/topProduct', fetchTopProduct)
+router.get('/newProduct', fetchNewProduct)
+
+router.route('/:id')
+.get(fetchProductById)
+.put(authenticate, authorizeAdmin, formidable(), updateProduct)
+.delete(authenticate, authorizeAdmin, removeProduct)
+
+export default router
